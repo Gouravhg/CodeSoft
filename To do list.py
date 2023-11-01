a6ea5a8468ddfcd0a99e047179f3cc2e3c4407a5
Gouravhg <144928115+Gouravhg@users.noreply.github.com>
2023-11-01 19:27:50 +0530

    Add files via upload

---
+++ To do list.py
@@ -0,0 +1,34 @@
+import tkinter as tk
+from tkinter import messagebox
+
+def add_task():
+    task = entry.get()
+    if task:
+        listbox.insert(tk.END, task)
+        entry.delete(0, tk.END)
+    else:
+        messagebox.showwarning("Warning", "Please enter a task!")
+
+def delete_task():
+    try:
+        selected_task_index = listbox.curselection()[0]
+        listbox.delete(selected_task_index)
+    except IndexError:
+        messagebox.showwarning("Warning", "Please select a task to delete!")
+
+root = tk.Tk()
+root.title("To-Do List")
+
+entry = tk.Entry(root, width=40)
+entry.pack(pady=10)
+
+add_button = tk.Button(root, text="Add Task", command=add_task)
+add_button.pack(side=tk.LEFT, padx=10)
+
+delete_button = tk.Button(root, text="Delete Task", command=delete_task)
+delete_button.pack(side=tk.LEFT)
+
+listbox = tk.Listbox(root, width=50)
+listbox.pack(pady=20)
+
+root.mainloop()
